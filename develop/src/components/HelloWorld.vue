1bd98a5db0a17820ba6d248bc5efe9f59c419c14
davids <dheo0@naver.com>
2020-10-19 00:06:37 +0900

    upload

---
+++ develop/src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <button @click="count++">count is: {{ count }}</button>
+  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+  <p>회원가입 - 등록, 수정</p>
+  <p>로그인 -일반로그인, 소셜로그인</p>
+  <p>게시판 - list, view, modify</p>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
